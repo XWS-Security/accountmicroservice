9f7db9f8c41b374eb7ed7456861c98b7c3a03725
lukaDoric <luka.doric@uns.ac.rs>
2021-03-21 14:18:42 +0100

    Get all and CA certificates.

--- src/main/java/com/example/pki/service/CertificateService.java
+++ src/main/java/com/example/pki/service/CertificateService.java
@@ -1,11 +1,23 @@
 package com.example.pki.service;
 
+import com.example.pki.model.OCSPCertificate;
 import com.example.pki.model.dto.CertificateDto;
+
+import java.security.cert.X509Certificate;
+import java.util.List;
 
 public interface CertificateService {
     void generateCertificate(CertificateDto dto);
+
     void changeCertificateStatus(String certificateAlias);
+
     boolean isAnyInChainRevoked(String certificateAlias);
+
     boolean isAnyInChainOutdated(String certificateAlias);
+
     boolean isCertificateValid(String certificateAlias);
+
+    Iterable<OCSPCertificate> getAllCertificates();
+
+    List<X509Certificate> getCACertificates();
 }
