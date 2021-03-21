366a851963158bd9fb26bb4cd5c972ceb1ae28e5
lukaDoric <luka.doric@uns.ac.rs>
2021-03-21 13:57:21 +0100

    Check certificate validity.

--- src/main/java/com/example/pki/service/CertificateService.java
+++ src/main/java/com/example/pki/service/CertificateService.java
@@ -4,4 +4,8 @@ import com.example.pki.model.dto.CertificateDto;
 
 public interface CertificateService {
     void generateCertificate(CertificateDto dto);
+    void changeCertificateStatus(String certificateAlias);
+    boolean isAnyInChainRevoked(String certificateAlias);
+    boolean isAnyInChainOutdated(String certificateAlias);
+    boolean isCertificateValid(String certificateAlias);
 }
