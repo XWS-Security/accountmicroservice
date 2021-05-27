dd5467bbae25cb423aa40e74ea60e1908a88b84a
lukaDoric <luka.doric@uns.ac.rs>
2021-05-27 14:05:06 +0200

    Microservice communication example.

--- src/main/java/com/example/pki/controller/FollowerMicroserviceController.java
+++ src/main/java/com/example/pki/controller/FollowerMicroserviceController.java
@@ -1,12 +1,17 @@
 package com.example.pki.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 
+@RestController
+@RequestMapping(value = "/followerMicroservice", produces = MediaType.APPLICATION_JSON_VALUE)
 public class FollowerMicroserviceController {
 
     @Value("${FOLLOWER}")
